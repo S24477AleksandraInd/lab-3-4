f3711e5d3d2e5c0beb1db48747e6ba6b79d031a1
Aleksandra Indulska <s24477@pjwstk.edu.pl>
2024-10-26 20:33:24 +0200

    feat: output top 5 models

--- asi_labs/lab3/learn.py
+++ asi_labs/lab3/learn.py
@@ -1,18 +1,8 @@
 from pycaret.regression import setup
 from .dataset import dataset
+from pathlib import Path
 
-# dataset = dataset[["unemp", "wage", "distance", "score"]]
-
-dataset = dataset.drop(columns=["rownames"])
-
-# convert fcollege mcollege	home	urban into yes/no into booleans
-
-dataset["fcollege"] = dataset["fcollege"].apply(lambda x: x == "yes")
-dataset["mcollege"] = dataset["mcollege"].apply(lambda x: x == "yes")
-dataset["home"] = dataset["home"].apply(lambda x: x == "yes")
-dataset["urban"] = dataset["urban"].apply(lambda x: x == "yes")
-
-print(dataset)
+# print(dataset)
 
 s = setup(
     data=dataset,
@@ -20,6 +10,7 @@ s = setup(
     session_id=33,
     categorical_features=["gender", "ethnicity", "income", "region"],
     numeric_features=["unemp", "wage", "distance", "tuition", "education"],
+    verbose=False,
     # use_gpu=True,
     # log_experiment="mlflow",
     # log_data=True,
@@ -31,14 +22,16 @@ models = s.compare_models(include=["lr", "lasso", "en", "rf", "xgboost"])
 
 best = s.get_leaderboard()
 
-print(best)
+# print(best)
 
 eval_res = s.evaluate_model(best["Model"])
 
-print(eval_res)
+# print(eval_res)
 
-
-# print pandas profile
-# profile = s._profile()
-
-# s.create_api(best["Model"], "Best")
+for _, row in best.iterrows():
+    Path(".models").mkdir(exist_ok=True)
+    s.save_model(row["Model"], f'.models/{row["Model Name"]}')
+    p = s.plot_model(row["Model"], save=True, verbose=False)
+    Path(p).rename(f".models/{row['Model Name']} Residuals.png")
+    p = s.plot_model(row["Model"], plot="error", save=True, verbose=False)
+    Path(p).rename(f".models/{row['Model Name']} Error.png")

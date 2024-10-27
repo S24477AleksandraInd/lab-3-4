4787415d63e696bf8591c8057dd3641a51a2098d
Aleksandra Indulska <s24477@pjwstk.edu.pl>
2024-10-27 18:20:40 +0100

    feat: add report creation

--- asi_labs/lab3/learn.py
+++ asi_labs/lab3/learn.py
@@ -1,7 +1,12 @@
 from pycaret.regression import setup
-from .dataset import dataset, dataset_test
+from .dataset import dataset, dataset_test, dataset_raw
 from pathlib import Path
+import jinja2
 import os
+import datetime
+import pdfkit
+import shutil
+import base64
 
 s = setup(
     data=dataset,
@@ -38,25 +43,72 @@ dt        0.4109  5.1157     0.099
 par       0.4502  4.9982     0.093
 """
 
+start_time = datetime.datetime.now()
 model = s.create_model("lar")
 tuned = s.tune_model(model, n_iter=24 * 24 * 6, optimize="RMSE")
+end_time = datetime.datetime.now()
 
-# Compare models
-# models = compare_models(n_select=3)
+tt = end_time - start_time
 
-# Tune top 3 models
-# tuned_models = [tune_model(model) for model in models]
-
-# Blend tuned models
-# blended_model = blend_models(tuned_models)
 final_model = tuned
 
 # Print final model
 print(final_model)
 
-Path(".models").mkdir(exist_ok=True)
-s.save_model(final_model, ".models/output_model")
-p = s.plot_model(final_model, save=True, verbose=False)
-Path(p).rename(".models/Residuals.png")
-p = s.plot_model(final_model, plot="error", save=True, verbose=False)
-Path(p).rename(".models/Error.png")
+images = [
+    s.plot_model(final_model, save=True, verbose=False),
+    s.plot_model(final_model, plot="error", save=True, verbose=False),
+    s.plot_model(final_model, plot="feature_all", save=True, verbose=False),
+    s.plot_model(final_model, plot="learning", save=True, verbose=False),
+    s.plot_model(final_model, plot="vc", save=True, verbose=False),
+]
+
+for image in images:
+    target_path = Path(".models") / Path(image).name
+    if target_path.exists():
+        target_path.unlink()
+    shutil.move(image, ".models")
+
+images = [".models/" + Path(image).name for image in images]
+
+# make a html report
+templates = Path(__file__).parent / "templates"
+env = jinja2.Environment(loader=jinja2.FileSystemLoader(templates))
+template = env.get_template("report.html")
+
+# open images and convert them to base64
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as fi:
+        return base64.b64encode(fi.read()).decode("utf8")
+
+images_base64 = { f"{Path(image).name}_base64": image_to_base64(image) for image in images }
+print(images_base64.keys())
+
+# data for pull
+s.predict_model(final_model, dataset_test)
+
+html = template.render(
+    dataset=dataset,
+    dataset_test=dataset_test,
+    dataset_raw=dataset_raw,
+    generated_at=datetime.datetime.now().isoformat(),
+    images=images_base64,
+    metrics=s.pull().drop(columns=["Model"]),
+    training_time=tt,
+    model_name=str(final_model),
+)
+
+with open(".models/report.html", "w", encoding="utf8") as f:
+    f.write(html)
+
+pdfkit.from_string(
+    html,
+    ".models/report.pdf",
+    options={
+        "encoding": "UTF-8",
+        "page-size": "A4",
+        "orientation": "Landscape",
+        "enable-local-file-access": True,
+    },
+)

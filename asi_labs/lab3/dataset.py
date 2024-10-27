833ae00c2ed8a72e4facf8c12237e2ef3b053eb4
Aleksandra Indulska <s24477@pjwstk.edu.pl>
2024-10-27 16:42:50 +0100

    feat: update dataset encodings

--- asi_labs/lab3/dataset.py
+++ asi_labs/lab3/dataset.py
@@ -1,14 +1,30 @@
 import pandas as pd
+from sklearn.preprocessing import RobustScaler
 
 dataset_raw = pd.read_csv(".assets/CollegeDistance.csv")
 
-# drop the rownames column
-
+# drop unused columns
 dataset = dataset_raw.drop(columns=["rownames"])
 
-# convert fcollege mcollege	home	urban into yes/no into booleans
+# encode gender
+dataset["gender"] = dataset["gender"].apply(lambda v: 1 if v == "male" else 0)
 
-dataset["fcollege"] = dataset["fcollege"].apply(lambda x: x == "yes")
-dataset["mcollege"] = dataset["mcollege"].apply(lambda x: x == "yes")
-dataset["home"] = dataset["home"].apply(lambda x: x == "yes")
-dataset["urban"] = dataset["urban"].apply(lambda x: x == "yes")
+# encode ethnicity
+dataset = pd.get_dummies(
+    dataset, columns=["ethnicity"], prefix=["ethnicity"], prefix_sep="_", dtype=int
+)
+
+# scale data
+scaler = RobustScaler()
+dataset["education"] = dataset["education"].astype(float)
+scalable_columns = dataset.select_dtypes(include=["float64"]).columns
+dataset[scalable_columns] = scaler.fit_transform(dataset[scalable_columns])
+
+# Create a test dataset
+dataset_test = dataset.sample(frac=0.3, random_state=7312)
+
+if __name__ == "__main__":
+    import pandas_profiling as pp
+
+    report = pp.ProfileReport(dataset)
+    report.to_file(".assets/CollegeDistance.html")

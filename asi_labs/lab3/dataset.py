3f119bd3897fec8c3732de8ede4713291b9c1448
Aleksandra Indulska <s24477@pjwstk.edu.pl>
2024-11-01 12:12:59 +0100

    fix: remove typo

--- asi_labs/lab3/dataset.py
+++ asi_labs/lab3/dataset.py
@@ -10,15 +10,15 @@ dataset = dataset_raw.drop(columns=["rownames"])
 dataset["gender"] = dataset["gender"].apply(lambda v: 1 if v == "male" else 0)
 
 # encode ethnicity
-dataset = pd.get_dummies(
-    dataset, columns=["ethnicity"], prefix=["ethnicity"], prefix_sep="_", dtype=int
-)
+# dataset = pd.get_dummies(
+#     dataset, columns=["ethnicity"], prefix=["ethnicity"], prefix_sep="_", dtype=int
+# )
 
 # scale data
-scaler = RobustScaler()
-dataset["education"] = dataset["education"].astype(float)
-scalable_columns = dataset.select_dtypes(include=["float64"]).columns
-dataset[scalable_columns] = scaler.fit_transform(dataset[scalable_columns])
+# scaler = RobustScaler()
+# dataset["education"] = dataset["education"].astype(float)
+# scalable_columns = dataset.select_dtypes(include=["float64"]).columns
+# dataset[scalable_columns] = scaler.fit_transform(dataset[scalable_columns])
 
 # Create a test dataset
 dataset_test = dataset.sample(frac=0.3, random_state=7312)

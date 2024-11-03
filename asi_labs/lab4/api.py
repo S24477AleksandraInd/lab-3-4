890d20c12b7c2eb8fa380b95bf90f4656c015e06
Aleksandra Indulska <s24477@pjwstk.edu.pl>
2024-11-03 17:18:55 +0100

    feat: redirect to docs

--- asi_labs/lab4/api.py
+++ asi_labs/lab4/api.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from pycaret.regression import load_model, predict_model
 import pandas as pd
-from asi_labs.lab3.dataset import dataset
+from asi_labs.lab3.dataset import dataset, transform_df
 import typing
 
 app = FastAPI()
@@ -25,6 +26,11 @@ class InputsModel(BaseModel):
     region: str
 
 
+@app.get("/")
+def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
+
 @app.get("/sample", summary="Get a sample of inputs")
 def read_inputs(count: int = 1):
     return dataset.drop(columns=["score"]).sample(count).to_dict(orient="records")
@@ -33,7 +39,8 @@ def read_inputs(count: int = 1):
 @app.post("/predict", summary="Predict the score")
 def predict(inputs: list[dict]):
     df = pd.DataFrame(inputs)
-    results = predict_model(model, df)
+    df_safe = transform_df(df)
+    results = predict_model(model, df_safe)
     results = results.rename(columns={"prediction_label": "score"})
 
     return results.to_dict(orient="records")

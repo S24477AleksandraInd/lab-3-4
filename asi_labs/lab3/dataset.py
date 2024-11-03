33c469b265c1dc6701920c6c360bb7e55a407022
Aleksandra Indulska <s24477@pjwstk.edu.pl>
2024-11-03 17:18:44 +0100

    feat: make a transform function

--- asi_labs/lab3/dataset.py
+++ asi_labs/lab3/dataset.py
@@ -3,22 +3,28 @@ from sklearn.preprocessing import RobustScaler
 
 dataset_raw = pd.read_csv("assets/CollegeDistance.csv")
 
+
+def transform_df(df: pd.DataFrame):
+    df["gender"] = df["gender"].apply(lambda v: 1 if v == "male" else 0)
+
+    # encode ethnicity
+    df = pd.get_dummies(
+        df, columns=["ethnicity"], prefix=["ethnicity"], prefix_sep="_", dtype=int
+    )
+
+    # scale data
+    scaler = RobustScaler()
+    df["education"] = df["education"].astype(float)
+    scalable_columns = df.select_dtypes(include=["float64"]).columns
+    df[scalable_columns] = scaler.fit_transform(df[scalable_columns])
+
+    return df
+
+
 # drop unused columns
 dataset = dataset_raw.drop(columns=["rownames"])
 
-# encode gender
-dataset["gender"] = dataset["gender"].apply(lambda v: 1 if v == "male" else 0)
-
-# encode ethnicity
-dataset = pd.get_dummies(
-    dataset, columns=["ethnicity"], prefix=["ethnicity"], prefix_sep="_", dtype=int
-)
-
-# scale data
-scaler = RobustScaler()
-dataset["education"] = dataset["education"].astype(float)
-scalable_columns = dataset.select_dtypes(include=["float64"]).columns
-dataset[scalable_columns] = scaler.fit_transform(dataset[scalable_columns])
+dataset = transform_df(dataset)
 
 # Create a test dataset
 dataset_test = dataset.sample(frac=0.3, random_state=7312)
